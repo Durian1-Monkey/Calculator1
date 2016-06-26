f66c57c5820e187d2ab188010f90c2e8ae5a8e51
Morita Takahiro <morita657@gmail.com>
2016-06-26 23:31:55 +0700

    Remove Comments

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,48 +1,44 @@
 /**
  * Created by morita on 6/26/2016 AD.
  */
-
-    var app = angular.module('calculator', []);
-	app.controller('calController', ['$scope', '$log', '$parse', function($scope) {
-      $scope.num =0;
-      $scope.calController = function($log) {
+var app = angular.module('calculator', []);
+app.controller('calController', ['$scope', function($scope) {
+    $scope.num = 0;
+    $scope.calController = function($log) {
         $log.debug('Test');
-      }
+    }
 
-      $scope.number = function(numb) {
+    $scope.number = function(numb) {
         $scope.num += String(numb);
-      }
+    }
 
-      $scope.plus = function(operator){
+    $scope.plus = function(operator) {
         $scope.num += String(operator);
-          console.log(operator, "is clicked.");
-      }
+        console.log(operator, "is clicked.");
+    }
 
-      $scope.subtract = function(operator){
+    $scope.subtract = function(operator) {
         $scope.num += String(operator);
-          console.log(operator, "is clicked.");
-      }
+        console.log(operator, "is clicked.");
+    }
 
-      $scope.divide = function(operator){
+    $scope.divide = function(operator) {
         $scope.num += String("/");
-          console.log(operator, "is clicked.");
-      }
-      $scope.operator = function(operator) {
+        console.log(operator, "is clicked.");
+    }
+
+    $scope.operator = function(operator) {
         if (operator == "=") {
-          console.log(operator, "is clicked.");
-          console.log($scope.num);
-          console.log(typeof $scope.num);
-          $scope.num = eval($scope.num);
-          console.log("This is the type of " + typeof $scope.num);
-          console.log(parseInt($scope.num));
+            console.log(operator, "is clicked.");
+            console.log($scope.num);
+            $scope.num = eval($scope.num);
         }
         if (operator == "X") {
-          $scope.num += String("*");
-          console.log(operator, "is clicked.");
+            $scope.num += String("*");
+            console.log(operator, "is clicked.");
+        } else if (operator == "C") {
+            $scope.num = " ";
+            console.log(operator, "is clicked.");
         }
-        else if (operator == "C") {
-          $scope.num = " ";
-          console.log(operator, "is clicked.");
-        }
-      }
-    }]);
+    }
+}]);
