aa08711e76103ba523e63b10e6676df71251dedf
Morita Takahiro <morita657@gmail.com>
2016-06-27 19:22:23 +0700

    Fix bug

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -2,43 +2,62 @@
  * Created by morita on 6/26/2016 AD.
  */
 var app = angular.module('calculator', []);
-app.controller('calController', ['$scope', function($scope) {
-    $scope.num = 0;
-    $scope.calController = function($log) {
+app.controller('calController', function ($scope, $log) {
+    "ngInject";
+    $scope.num = ' ';
+    $scope.calController = function ($log) {
         $log.debug('Test');
     }
 
-    $scope.number = function(numb) {
+    $scope.number = function (numb) {
+        $log.debug("number");
         $scope.num += String(numb);
     }
 
-    $scope.plus = function(operator) {
+    $scope.plus = function (operator) {
         $scope.num += String(operator);
-        console.log(operator, "is clicked.");
+        $log.debug(operator, "is clicked.");
     }
 
-    $scope.subtract = function(operator) {
+    $scope.subtract = function (operator) {
         $scope.num += String(operator);
-        console.log(operator, "is clicked.");
+        $log.debug(operator, "is clicked.");
     }
 
-    $scope.divide = function(operator) {
+    $scope.divide = function (operator) {
         $scope.num += String("/");
-        console.log(operator, "is clicked.");
+        $log.debug(operator, "is clicked.");
     }
 
-    $scope.operator = function(operator) {
-        if (operator == "=") {
-            console.log(operator, "is clicked.");
+    $scope.multiple = function (operator) {
+        $scope.num += String("*");
+        $log.debug(operator, "is clicked.");
+    }
+
+    $scope.operator = function (operator) {
+        if (operator === "=") {
             console.log($scope.num);
-            $scope.num = eval($scope.num);
+            console.log($scope.num[1]);
+            if ($scope.num[1] === "*" | "/" && $scope.num[0] === "*" | "/" ) {
+                console.log("no number is before * or / operators.");
+                $scope.num = 0;
+            }
+            else {
+                $log.debug(operator, "is clicked.");
+                $log.debug($scope.num);
+                $scope.num = eval($scope.num);
+                $log.debug($scope.num);
+            }
         }
-        if (operator == "X") {
-            $scope.num += String("*");
-            console.log(operator, "is clicked.");
-        } else if (operator == "C") {
+        if (operator === "C") {
             $scope.num = " ";
-            console.log(operator, "is clicked.");
+            $log.debug(operator, "is clicked.");
         }
     }
-}]);
+});
+
+app.directive('numberButton', function () {
+    return {
+        templateUrl: 'template/numberButton.html'
+    };
+});
